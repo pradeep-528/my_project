65de002654483de4f7a4f28bf24017b8d3821959
v pradeep kumar raju <vpradeep.raj@gmail.com>
2013-04-10 14:37:16 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -108,7 +108,7 @@ ROOT_URLCONF = 'mysite.urls'
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 TEMPLATE_DIRS = (
-'/home/pradeep/my_project/blog/templates',
+'/home/pradeep/my_project1/blog/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -156,10 +156,14 @@ LOGGING = {
         },
     }
 }
-
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+import os
+
+if not os.environ.has_key('DATABASE_URL'):
+   os.environ['DATABASE_URL'] = 'postgres://pradeep:password@localhost/postgres_db'
+DATABASES['default'] =  dj_database_url.config(default=os.environ['DATABASE_URL'])
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
